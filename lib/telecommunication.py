2b7e26c4b7eb20a088bca21694eab25f6e6958db
Jarosław Glegoła <jglegola@mion.elka.pw.edu.pl>
2019-05-31 19:02:06 +0200

    Added converter from genom to result

--- lib/telecommunication.py
+++ lib/telecommunication.py
@@ -1,8 +1,13 @@
 from dataclasses import dataclass
 from typing import Set, List, Tuple
 from .graph import Graph
+# from .calculate_result import calculate_result
+from .pick_transponders import pick_transponders
+import numpy as np
+from itertools import combinations
 
 from random import randrange
+
 
 @dataclass(frozen=True)
 class Transponder():
@@ -36,6 +41,11 @@ class Demand():
     paths: List[Tuple[float, List[int]]]
 
 
+@dataclass
+class DemandResult():
+    transponders: List[Tuple[int, Transponder]]
+    demand: Demand
+
 
 @dataclass
 class Problem():
@@ -51,26 +61,27 @@ class Problem():
 
         return Genom(data, self)
 
+
 @dataclass
 class Genom():
     data: List[int]
     problem: Problem
 
-    #multi-point crossing
+    # multi-point crossing
     def crossing(self, other_genom):
         center = len(self.data) // 2
         point_1 = randrange(0, center)
         print(point_1)
         point_2 = randrange(center, len(self.data))
         print(point_2)
-        new_data = (self.data[0 : point_1] 
-                + other_genom.data[point_1 : point_2] 
-                + self.data[point_2 : len(self.data)] )
+        new_data = (self.data[0: point_1]
+                    + other_genom.data[point_1: point_2]
+                    + self.data[point_2: len(self.data)])
 
         return Genom(new_data, self.problem)
 
     def mutation(self):
-        random_int = randrange(0, len(self.data) )
+        random_int = randrange(0, len(self.data))
 
         upper_range = len(self.problem.demands[random_int].paths)
         new_data = self.data[:]
@@ -78,42 +89,72 @@ class Genom():
 
         return Genom(new_data, self.problem)
 
-        
+    def _update_band_map(self, band_map, frequency_needed, path):
+        for start, end in zip(path, path[1:]):
+            band_map[start, end] += frequency_needed
+            band_map[end, start] += frequency_needed
 
+    def _pick_frequencies_for_transponders(self, min_frequency, transponders):
+        current_frequency = min_frequency
+        transponders_with_frequencies = []
+        for transponder in transponders:
+            transponders_with_frequencies.append(
+                (current_frequency, transponder))
+            current_frequency += transponder.frequency_width
 
+        return transponders_with_frequencies
 
-    
+    def _pick_band_cost(self, frequency, bands):
+        if frequency == 1:
+            return 0
 
-# '''
-# This class represents one specific result for one specific
-# demand. Also has methods to check if the result is correct
-# i.e. that the power budget is not exceeded or demand is fulfilled
-# '''
-# @dataclass
-# class Result():
-#     id: int
-#     demand: Demand
-#     path: int
-#     # list of tuples (frequency, transponder)
-#     transponders: List[Tuple[int, Transponder]]
+        if bands[-1].frequency_range_to < frequency:
+            return float('inf')
 
-#     def power_budget_exceeded(self, bands: List[Band]):
-#         path_length = self.demand.paths[self.path][0]
-#         return any([
-#             path_length * band.loss_per_km > transponder.power_budget
-#             for frequency, transponder in self.transponders
-#             for band in bands
-#             if band.contains_frequency(frequency)
-#         ])
+        bands_sum = 0
+        for band in bands:
+            if band.frequency_range_from > frequency:
+                break
 
-#     def demand_not_fulfilled(self):
-#         return (sum([
-#             transponder.bitrate for _, transponder in self.transponders
-#         ]) < self.demand.bitrate)
+            bands_sum += band.cost
 
-#     def is_correct(self, bands: List[Band]) -> bool:
-#         return not (self.power_budget_exceeded(bands)
-#                     or self.demand_not_fulfilled())
+        return bands_sum
 
-#     def cost(self) -> float:
-#         return sum([transponder.cost for _, transponder in self.transponders])
+    def calculate_result(self, bands: List[Band], transponders: List[Transponder]):
+        """
+        calculate result based on data. returns 
+        tuple of [cost of whole result, band_map, demands with frequencies]
+        cost -> float
+        band_map -> 2D nd_array with values as lowest free frequency on an edge
+        demands with frequencies -> List[DemandResult]
+        """
+        demands_result = []
+        graph = self.problem.graph
+        band_map = np.ones([graph.dimension, graph.dimension], dtype=int)
+
+        for index, demand in enumerate(self.problem.demands):
+            path_length, path = demand.paths[self.data[index]]
+            picked_transponders = pick_transponders(
+                demand.bitrate, path_length, transponders, bands[-1])
+
+            frequency_width_needed = sum(
+                [t.frequency_width for t in picked_transponders])
+
+            min_frequency = max([band_map[start, end]
+                                 for start, end in zip(path, path[1:])])
+
+            transponders_with_frequencies = self._pick_frequencies_for_transponders(
+                min_frequency, picked_transponders)
+
+            self._update_band_map(band_map, frequency_width_needed, path)
+
+            demands_result.append(DemandResult(
+                transponders_with_frequencies, demand))
+
+        transponders_cost = sum([transponder.cost for demand_result in demands_result
+                                 for _, transponder in demand_result.transponders])
+
+        bands_cost = sum([self._pick_band_cost(band_map[start, end], bands)
+                          for start, end in combinations(range(self.problem.graph.dimension), 2)])
+
+        return transponders_cost + bands_cost, band_map, demands_result
--- test/pick_transponder_test.py
+++ test/pick_transponder_test.py
@@ -1,5 +1,5 @@
 import unittest
-from models.models import Transponder, Band, Demand, Result
+from lib.telecommunication import Transponder, Band, Demand
 from lib.pick_transponders import pick_transponders
 from lib.graphical import Graphical
 
@@ -13,56 +13,76 @@ class TSPTests(unittest.TestCase):
                         cost=5, frequency_width=3)
         ]
 
+        self.band = Band(id=2, cost=2, frequency_range_from=385,
+                         frequency_range_to=768, loss_per_km=0.055)
+
     def test_single_transponder(self):
         demand = Demand(id=1, node1=1, node2=2, bitrate=100,
-                        paths=[(1000, [1, 2]), (14, [1, 3, 2])])
+                        paths=[(10, [1, 2]), (14, [1, 3, 2])])
 
-        transponders = pick_transponders(demand, self.transponders)
+        transponders = pick_transponders(
+            demand.bitrate, demand.paths[0][0], self.transponders, self.band)
 
         self.assertCountEqual(transponders, [self.transponders[1]])
 
     def test_single_transponder1(self):
         demand = Demand(id=1, node1=1, node2=2, bitrate=40,
-                        paths=[(1000, [1, 2]), (14, [1, 3, 2])])
+                        paths=[(10, [1, 2]), (14, [1, 3, 2])])
 
-        transponders = pick_transponders(demand, self.transponders)
+        transponders = pick_transponders(
+            demand.bitrate, demand.paths[0][0], self.transponders, self.band)
 
         self.assertCountEqual(transponders, [self.transponders[0]])
 
     def test_single_transponder2(self):
         demand = Demand(id=1, node1=1, node2=2, bitrate=41,
-                        paths=[(1000, [1, 2]), (14, [1, 3, 2])])
+                        paths=[(10, [1, 2]), (14, [1, 3, 2])])
 
-        transponders = pick_transponders(demand, self.transponders)
+        transponders = pick_transponders(
+            demand.bitrate, demand.paths[0][0], self.transponders, self.band)
 
         self.assertCountEqual(transponders, [self.transponders[1]])
 
     def test_multiple_transponders(self):
         demand = Demand(id=1, node1=1, node2=2, bitrate=101,
-                        paths=[(1000, [1, 2]), (14, [1, 3, 2])])
+                        paths=[(10, [1, 2]), (14, [1, 3, 2])])
 
-        transponders = pick_transponders(demand, self.transponders)
+        transponders = pick_transponders(
+            demand.bitrate, demand.paths[0][0], self.transponders, self.band)
 
         self.assertCountEqual(
             transponders, [self.transponders[1], self.transponders[0]])
 
     def test_multiple_transponders1(self):
         demand = Demand(id=1, node1=1, node2=2, bitrate=200,
-                        paths=[(1000, [1, 2]), (14, [1, 3, 2])])
+                        paths=[(10, [1, 2]), (14, [1, 3, 2])])
 
-        transponders = pick_transponders(demand, self.transponders)
+        transponders = pick_transponders(
+            demand.bitrate, demand.paths[0][0], self.transponders, self.band)
 
         self.assertCountEqual(
             transponders, [self.transponders[1], self.transponders[1]])
 
     def test_multiple_transponders2(self):
         demand = Demand(id=1, node1=1, node2=2, bitrate=201,
-                        paths=[(1000, [1, 2]), (14, [1, 3, 2])])
+                        paths=[(10, [1, 2]), (14, [1, 3, 2])])
 
-        transponders = pick_transponders(demand, self.transponders)
+        transponders = pick_transponders(
+            demand.bitrate, demand.paths[0][0], self.transponders, self.band)
 
         self.assertCountEqual(transponders, [
-                             self.transponders[1], self.transponders[1], self.transponders[0]])
+            self.transponders[1], self.transponders[1], self.transponders[0]])
+
+    def test_single_transponders_with_power_exceeded(self):
+        demand = Demand(id=1, node1=1, node2=2, bitrate=201,
+                        paths=[(200, [1, 2]), (14, [1, 3, 2])])
+
+        transponders = pick_transponders(
+            demand.bitrate, demand.paths[0][0], self.transponders, self.band)
+
+        self.assertCountEqual(transponders, [
+            self.transponders[1], self.transponders[1], self.transponders[1]])
+
 
 
 if __name__ == '__main__':
